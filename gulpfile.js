cc90e71db5ec82c7a90326e3349a9ee88d330a1b
guoliang <guoliang@danlu.com>
2016-08-12 14:02:25 +0800

    调整less文件结构

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,16 @@
 var gulp = require('gulp');
-// var sass = require('gulp-sass');
 var less = require('gulp-less');
 var autoprefixer = require('gulp-autoprefixer');
 
-// 一次性编译 Sass
+// less.render
 gulp.task('less', function() {
     return gulp.src('./source/less/*.less')
-        .pipe(less({outputStyle: 'compressed'}))
+        .pipe(less({compress: true}))
         .pipe(autoprefixer())
         .pipe(gulp.dest('./source/css'));
 });
 
-// 实时编译
+// liverender
 gulp.task('liveless', ['less'], function() {
     gulp.watch('./source/less/_partial/*.less', ['less']);
     gulp.watch('./source/less/*.less', ['less']);
