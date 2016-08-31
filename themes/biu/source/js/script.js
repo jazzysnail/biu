4e904697419ca01cce6f059947f7158a39e689eb
guoliang <guoliang@danlu.com>
2016-08-31 18:56:57 +0800

    2016年8月31日18:56:54

--- themes/biu/source/js/script.js
+++ themes/biu/source/js/script.js
@@ -1,12 +1,50 @@
-var headroom = new Headroom(document.getElementById('header'), {
-  tolerance: 10,
-  offset: 500,
-  classes: {
-  	initial : "animated",
-    pinned: "slide-up",
-    unpinned: "slide-down"
-  }
-});
-headroom.init();
-// to destroy
-// headroom.destroy();
+(function () {
+	//// 小屏处理
+	if (window.outerWidth < 1000) {
+		document.getElementById('profile').remove();
+		document.getElementsByClassName('logo-link').remove();
+	};
+	//// 隐藏索引
+	var offset = window.location.pathname == '/' ? 600 : 0;
+	var headroom = new Headroom(document.getElementById('header'), {
+	  tolerance: 10,
+	  offset: offset,
+	  classes: {
+	  	initial : "animated",
+	    pinned: "slide-up",
+	    unpinned: "slide-down"
+	  }
+	});
+	headroom.init();
+	// to destroy
+	// headroom.destroy();
+
+	//// 固定索引
+	var pinHeader = new pin("header");
+
+	/*
+	 * @constructor of pin
+	 * @argument {string} ele
+	 * @argument {number} offset ![Is not enabled]
+	*/
+	function pin (ele,offset) {
+		var self = this;
+		var ele = self.ele = document.getElementById(ele);
+		var offsetTop = getoffsetTop(ele);
+		var isIE6 = /msie 6/i.test(navigator.userAgent);
+		window.onscroll = function() {
+		  var bodyScrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+		  if (bodyScrollTop > offsetTop) {
+		    ele.style.position = (isIE6) ? "absolute" : "fixed";
+		    ele.style.top = (isIE6) ? bodyScrollTop + "px" : "0px";
+		  } else {
+		    ele.style.position = "static";
+		  }
+		}
+		function getoffsetTop(ele) {
+		  var offset = ele.offsetTop;
+		  if (ele.offsetParent != null) offset += getoffsetTop(ele.offsetParent);
+		  return offset;
+		}
+	}
+})();
